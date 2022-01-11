e0c8d85ffae6ca2d9fd94e463bb6fd9893956203
AnandhuRaj42 <93871968+AnandhuRaj42@users.noreply.github.com>
2022-01-11 21:37:44 +0530

    Add files via upload

---
+++ CO4/CO4_5.py
@@ -0,0 +1,30 @@
+class publisher:
+    def __init__(self,name):
+        self.name=name
+    def display1(self):
+        print("Publisher:",self.name)
+        
+class book(publisher):
+    def TA(self,title,author):
+        self.title=title
+        self.author=author
+        
+    def display1(self):
+        print("Title:",self.title)
+        print("Author:",self.author)
+        
+class python(book):
+    def __init__(self,name,price,no_of_page):
+        self.price=price
+        self.no_of_page=no_of_page
+        publisher.__init__(self,name)
+    def display(self):
+        print("Price:",self.price)
+        print("No Of Pages:",self.no_of_page)
+
+        
+obj=python(1000,500,"Van Bossom")
+obj.TA("Python","Van Bossom")
+obj.display1()
+obj.display()
+    
